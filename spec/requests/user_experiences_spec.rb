c0919b8e15f6cd232c21b1b7b1b41d1255bb47a1
Aaron1515 <aaron1515@gmail.com>
2016-06-21 22:42:07 -0700

    correct UX rspec path

--- spec/requests/user_experiences_spec.rb
+++ spec/requests/user_experiences_spec.rb
@@ -3,8 +3,9 @@ require 'rails_helper'
 RSpec.describe "UserExperiences", type: :request do
   describe "User can add names to field" do
     it "works! (now write some real specs)" do
-      get user_experiences_path
+      get welcome_index_path
       expect(response).to have_http_status(200)
+
     end
   end
 end
