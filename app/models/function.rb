7b1690627c94603238b6f98f25c1fc24921fa73e
Aaron1515 <aaron1515@gmail.com>
2016-07-01 13:02:57 -0700

    add game class

--- app/models/function.rb
+++ app/models/function.rb
@@ -1,3 +1,28 @@
-require relative 'player'
+require_relative 'player'
+
 class Game
+  attr_accessor :players
+  attr_reader :round
+
+  def initialize
+    @round = 0
+    @players = []
+
+  end
+
+  def add_player(player_name)
+    @players << Player.new(player_name)
+  end
+
+  def edit_score(num_to_change)
+  end
+
+  def round_up
+    @round += 1
+  end
+
+  def round_down
+    @round -= 1
+  end
+
 end
